52c606c7c59e6e4cd4e8e7f045085946178fc1d3
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2016-03-25 22:54:59 +0800

    Add script create-table.sql

---
+++ create-table.sql
@@ -0,0 +1,18 @@
+create table word (
+       id    bigserial,
+       context text,
+       vector text
+);
+
+create table bidword (
+       id    bigserial,
+       context text,
+       vector text
+);
+
+create table query (
+       id    bigserial,
+       context text,
+       vector text
+);
+
