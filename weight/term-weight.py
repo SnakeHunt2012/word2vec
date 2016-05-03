fee68a39e937f40584d3acbb28b08cc1b6af1726
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2016-05-03 14:34:16 +0800

    Update weight/term-weight.py

--- weight/term-weight.py
+++ weight/term-weight.py
@@ -3,6 +3,8 @@
 
 from codecs import open
 from argparse import ArgumentParser
+from math import log
+
 
 def load_entropy_dict(entropy_file):
 
@@ -38,14 +40,32 @@ def main():
             if len(splited_line) != 2:
                 continue
             phrase_str, phrase_seg = splited_line
-            word_entropy_list = [(token.split("")[0],
+            word_entropy_list = [[token.split("")[0],
                                   float(token.split("")[1]),
                                   float(left_entropy_dict[token.split("")[0]]) if token.split("")[0] in left_entropy_dict else None,
-                                  float(right_entropy_dict[token.split("")[0]]) if token.split("")[0] in right_entropy_dict else None)
+                                  float(right_entropy_dict[token.split("")[0]]) if token.split("")[0] in right_entropy_dict else None]
                                  for token in phrase_seg.split("")]
             if word_entropy_list is None or len(word_entropy_list) == 0:
                 continue
-            print " ".join(["%s/%s/%s/%s" % (word, weight, left_entropy, right_entropy) for word, weight, left_entropy, right_entropy in word_entropy_list])
+            cross_weight = None
+            if len(word_entropy_list) > 1:
+                #cross_weigt = [(post_word[2] / prev_word[3] if (post_word[2] != None and prev_word[3] != None and prev_word[3] > 0.0) else None,
+                #                post_word[3] / prev_word[2] if (post_word[2] != None and prev_word[3] != None and prev_word[2] > 0.0) else None)
+                #               for prev_word, post_word in zip(word_entropy_list[:-1], word_entropy_list[1:])]
+                cross_weight = [(None if (post_word[2] is None or prev_word[3] is None or prev_word[3] <= 0.0) else post_word[2] / prev_word[3],
+                                 None if (post_word[2] is None or prev_word[3] is None or post_word[2] <= 0.0) else prev_word[3] / post_word[2])
+                                for prev_word, post_word in zip(word_entropy_list[:-1], word_entropy_list[1:])]
+            if cross_weight is None:
+                print " ".join(["%s/%s/%s/%s" % (word, weight, left_entropy, right_entropy) for word, weight, left_entropy, right_entropy in word_entropy_list])
+            else:
+                length = len(word_entropy_list)
+                for index in xrange(length):
+                    if index != length - 1:
+                        print "%s/%s/%s/%s [%s/%s]" % (word_entropy_list[index][0], word_entropy_list[index][1], word_entropy_list[index][2], word_entropy_list[index][3], cross_weight[index][0], cross_weight[index][1]),
+                    else:
+                        print "%s/%s/%s/%s" % (word_entropy_list[index][0], word_entropy_list[index][1], word_entropy_list[index][2], word_entropy_list[index][3])
+                    
+                
     
 
 if __name__ == "__main__":
