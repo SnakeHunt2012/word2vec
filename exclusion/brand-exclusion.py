634f801b1e38ac4c5fae100cd2632c445d8be30c
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2016-05-11 14:58:03 +0800

    Modify brand-exclusion.py, follow input format modification (score added into format).

--- exclusion/brand-exclusion.py
+++ exclusion/brand-exclusion.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from re import compile
 from sys import getsizeof
 from time import time
 from codecs import open
@@ -47,12 +48,11 @@ def main():
     dict_file = args.dict_file
     sim_file = args.sim_file
 
-    print "loading dict_file ..."
     start_time = time()
     brand_set, category_dict = load_dict(dict_file)
     end_time = time()
-    print "loading dict_file done", duration(start_time, end_time)
 
+    spliter = compile("(?<=\/[0-9].[0-9][0-9][0-9][0-9][0-9][0-9]) ")    
     with open(sim_file, 'r') as fd:
         for line in fd:
             splited_line = line.strip().split("\t")
@@ -60,7 +60,8 @@ def main():
                 continue
             
             query = splited_line.pop(0)
-            bidword_list = "".join(splited_line).strip(";").split(";")
+            #bidword_list = "".join(splited_line).strip(";").split(";")
+            bidword_list = [('/'.join(bidword.split('/')[:-1]), bidword.split('/')[-1]) for bidword in spliter.split(splited_line.pop(-1))]
 
             query_brand_set = set()
             query_category_set = set()
@@ -71,26 +72,26 @@ def main():
 
             res_list = []
             exc_list = []
-            for bidword in bidword_list:
+            for bidword, score in bidword_list:
                 if len(set(bidword_seg for bidword_seg in bidword.split() if bidword_seg in category_dict) & query_brand_set) > 0:
-                    res_list.append(bidword)
+                    res_list.append("%s/%s" % (bidword, score))
                     break
                 is_exclusive = False
                 for bidword_seg in bidword.split():
                     if bidword_seg in category_dict:
                         if len(category_dict[bidword_seg] & query_category_set) > 0:
                             is_exclusive = True
-                            exc_list.append(bidword)
+                            exc_list.append("%s/%s" % (bidword, score))
                             break
                 if not is_exclusive:
-                    res_list.append(bidword)
+                    res_list.append("%s/%s" % (bidword, score))
 
             if BRAND_EXCLUDED:
                 if len(exc_list) > 0:
-                    print "%s\t%s" % (query, ";".join(exc_list))
+                    print "%s\t%s" % (query, " ".join(exc_list))
             else:
                 if len(res_list) > 0:
-                    print "%s\t%s" % (query, ";".join(res_list))
+                    print "%s\t%s" % (query, " ".join(res_list))
     
 if __name__ == "__main__":
 
