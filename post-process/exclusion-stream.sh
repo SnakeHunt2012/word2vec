b8bcc4119bb7bb50200513f63274f0951166c126
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2016-05-25 20:18:39 +0800

    Modify script exclusion-stream.sh, refine stream parameters.

--- post-process/exclusion-stream.sh
+++ post-process/exclusion-stream.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-INPUT=/home/hdp-guanggao/hive/warehouse/hdp_guanggao.db/w2v_query_bidword_sim_extension/ds=2016-05-16
-OUTPUT=/home/hdp-guanggao/huangjingwen/data/query-bidword-exclusion/ds=2016-05-16
+INPUT=/home/hdp-guanggao/hive/warehouse/hdp_guanggao.db/w2v_query_bidword_sim_extension/ds=2016-05-21
+OUTPUT=/home/hdp-guanggao/huangjingwen/data/query-bidword-extension/raw
+
 STREAMING=/usr/bin/hadoop/software/hadoop/contrib/streaming/hadoop-streaming.jar
 
 alias shadoop='sudo -E -u hdp-guanggao /usr/bin/hadoop/software/hadoop//bin/hadoop'
 shopt -s expand_aliases
+shadoop fs -test -e $OUTPUT
+if [ $? -eq 0 ]
+then
+    shadoop fs -rmr $OUTPUT
+fi
 
 shadoop jar $STREAMING \
 -D mapred.linerecordreader.maxlength=1024000 \
--D mapred.reduce.tasks=500 \
 -D mapred.job.priority=VERY_HIGH \
+-D mapred.reduce.tasks=0 \
 -D mapred.job.name='huangjingwen:query-bidword-exclusion' \
 -D map.output.key.field.separator='\t' \
 -D stream.num.map.output.key.fields=1 \
