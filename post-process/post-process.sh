1fbc2fcf36a2d2b9f64d7bfb6c035151e0fbf04a
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2016-06-02 16:18:13 +0800

    Remove sensitive information.

--- post-process/post-process.sh
+++ post-process/post-process.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 
 # post-process.sh
-# Written by Jingwen HUANG <huangjingwen@360.cn>
+# Written by Jingwen HUANG <SnakeHunt2012@gmail.com>
 # Post-process of the query->bidword procedure.
 #
 
